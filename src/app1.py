1dfd9615e504d31f7e41268babf47b4263342010
bruvio <bruno.viola@protonmail.com>
2021-06-02 19:59:57 +0100

    table report endpoint

--- src/app1.py
+++ src/app1.py
@@ -7,7 +7,7 @@ import dash_core_components as dcc
 import plotly.graph_objs as go
 import pandas as pd
 import sqlite3
-
+import numpy as np
 from src.server import server
 
 
@@ -42,17 +42,67 @@ colors = {
 name = "workouts_bruvio_2020.csv"
 
 # df = read_df_from_s3(name, bucket)
-df = pd.read_csv("workouts_bruvio_2020.csv")
+# df = pd.read_csv("workouts_bruvio_2020.csv")
 
-df_complete = df
-df_incomplete = df
+cnx = sqlite3.connect("./src/data.db")
+
+df = pd.read_sql_query("SELECT * FROM cartridges", cnx)
+
+
+is_complete = df["testStatus"] == "Complete"
+df_try = df[is_complete]
+df_fake = df.append([df_try] * 5, ignore_index=True)
+
+
+df_fake["submitedOn"] = pd.to_datetime(
+    df_fake["submissionDateTime"], infer_datetime_format=True
+)
+df_fake["testStartedOn"] = pd.to_datetime(
+    df_fake["testStartDateTime"], infer_datetime_format=True
+)
+df_fake["lastUpdatedon"] = pd.to_datetime(
+    df_fake["lastUpdatedDateTime"], infer_datetime_format=True
+)
+
+df_fake.drop(
+    ["submissionDateTime", "testStartDateTime", "lastUpdatedDateTime"],
+    axis=1,
+    inplace=True,
+)
+
+df_fake["testTime"] = (df_fake["lastUpdatedon"] - df_fake["submitedOn"]) / pd.Timedelta(
+    minutes=1
+)
+
+
+df_testStatus = pd.pivot_table(
+    df_fake, index="testStatus", values="cartridgeId", aggfunc=[len]
+)
+
+df_testStatus.reset_index(inplace=True)
+df_testStatus.columns = ["testStatus", "count"]
+
+
+p_table = pd.pivot_table(
+    df_fake,
+    index=["hospitalName", "pattern"],
+    values=["testTime"],
+    aggfunc={"testTime": [np.sum, np.mean]},
+)
+
+p_table.reset_index(inplace=True)
+
+p_table.columns = ["hospitalName", "pattern", "testTime_mean", "testTime_sum"]
+
 
 # Create your connection.
-# cnx = sqlite3.connect('./src/data.db')
 
-# df = pd.read_sql_query("SELECT * FROM cartridges", cnx)
 
-dataframes = {"df_complete": df_complete, "df_incomplete": df_incomplete}
+dataframes = {
+    "allData": df_fake,
+    "df_testStatus": df_testStatus,
+    "df_testingTime": p_table,
+}
 
 
 def get_data_object(user_selection):
@@ -69,7 +119,7 @@ app.layout = html.Div(
         dcc.Dropdown(
             id="field-dropdown",
             options=[{"label": df, "value": df} for df in dataframes],
-            value="df",
+            value="allData",
             clearable=False,
         ),
         dash_table.DataTable(id="table"),
